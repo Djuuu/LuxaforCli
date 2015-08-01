020eba0215ca81f05e0fd01502daa2b425a41b30
Djuuu <julien@tardot.net>
2015-08-01 21:04:52 +0200

    First interaction (SetColor on all leds)

--- LuxaforCli/Program.cs
+++ LuxaforCli/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LuxaforSharp;
 
 namespace LuxaforCli
 {
@@ -10,6 +11,42 @@ namespace LuxaforCli
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("LuxaforCLI needs at least 1 argument");
+                return;
+            }
+            
+            // color
+            ColorParser colorParser = new ColorParser(args[0]);
+            if (colorParser.color.IsEmpty)
+            {
+                Console.WriteLine(colorParser.error);                
+                return;
+            }
+            Color color = new Color(colorParser.color.R, colorParser.color.G, colorParser.color.B);
+
+            // device
+            IDeviceList list = new DeviceList();
+            list.Scan();
+            if (list.Count() == 0) 
+            {
+                Console.WriteLine("Device not found.");
+                return;
+            }
+
+            IDevice device = list.First();
+
+
+            // device action
+            device.SetColor(LedTarget.All, color, null, 1000);
+
+
+
+            // seems needed
+            System.Threading.Thread.Sleep(25);
+
+            device.Dispose();
         }
     }
 }
